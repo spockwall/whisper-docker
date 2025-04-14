87a0801af6c4410576bb8fc0142850e91d062d89
ben0024 <ben@sudoresearch.com>
2025-04-15 01:19:16 +0800

    feat: modify script and readme

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-```shellscript=
-$ cd whisper
-$ docker-compose build
-$ docker-compose run whisper -o "./app/outputs" python main.py
-```
+# Openai-whisper
+
+## Description
+Running openai-whisper in docker container
+
+
+## How to use 
+1. Enter directory: "/whisper"
+2. Put the audio files in /audios, file types can be mp3, mp4, wav
+3. change the audio file names in main.py
+4. Run the following commands
+    ```shellscript=
+    $ cd whisper
+    $ docker-compose build
+    $ docker-compose run whisper python main.py
+    ```
+5. Waiting ....
+6. check the output files in "/outputs"
+
+## Notice
+1. Do not modify "/models", "docker-compose.yml", and "Dockerfile" 
+2. Do not change the relative positions of the files and folders.
--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import whisper
+import os
+import subprocess
 
 MODEL_ROOT = "/app/models"
 AUDIO_ROOT = "/app/audios"
+OUTPUT_ROOT = "/app/outputs"
 AUDIO_FILE = "test.mp3"
+MODLE_NAME = "small"  # tiny, base, small, medium, large
+LANGUAGE = "Chinese"  # Chinese, English
 
-
-model = whisper.load_model("small", download_root=MODEL_ROOT)
-
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio(AUDIO_ROOT + "/" + AUDIO_FILE)
-audio = whisper.pad_or_trim(audio)
-
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
-
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
-
-# decode the audio
-options = whisper.DecodingOptions()
-result = whisper.decode(model, mel, options)
-
-# print the recognized text
-print(result.text)
+subprocess.run(
+    [
+        "whisper",
+        AUDIO_ROOT + "/" + AUDIO_FILE,
+        "--model",
+        MODLE_NAME,
+        "--model_dir",
+        MODEL_ROOT,
+        "--output_dir",
+        OUTPUT_ROOT,
+        "--language",
+        LANGUAGE,
+    ]
+)
