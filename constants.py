29e6c561b79795b657eab81338491ae0b49be338
ben0024 <ben@sudoresearch.com>
2025-04-15 18:57:52 +0800

    feat: add constants for use interface

--- constants.py
+++ constants.py
@@ -1,16 +1,15 @@
-from dataclasses import dataclass
-
+# Docker container paths
 MODEL_ROOT = "/app/models"
 AUDIO_ROOT = "/app/audios"
 OUTPUT_ROOT = "/app/outputs"
 LANGUAGE = "Chinese"  # Chinese, English
+CACHE_FILE = "/app/.cache.json"
+
+# User directories
+USER_AUDIO_DIR = "./audios"
+USER_OUTPUT_DIR = "./outputs"
+USER_CACHE_FILE = "./.cache.json"
+SERVICE_NAME = "whisper"
 
 
-@dataclass
-class Model:
-    # tiny, base, small, medium, large
-    tiny: str = "tiny"
-    base: str = "base"
-    small: str = "small"
-    medium: str = "medium"
-    large: str = "large"
+MODELS = ["tiny", "base", "small", "medium", "large"]
