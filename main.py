d1e443b8cc468ac3ab28317c316bb5e52c101e5e
ben0024 <ben@sudoresearch.com>
2025-04-15 19:34:52 +0800

    feat: add file exist existence check in main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-import whisper
+import json
 import os
 import subprocess
-from constants import MODEL_ROOT, AUDIO_ROOT, OUTPUT_ROOT, LANGUAGE, Model
+from constants import MODEL_ROOT, AUDIO_ROOT, OUTPUT_ROOT, LANGUAGE, CACHE_FILE
 
 
-def main(model_name: Model, file_name: str):
+def main():
+    with open(CACHE_FILE) as f:
+        config = json.load(f)
+    model_name = config["model_name"]
+    file_name = config["file_name"]
+
+    if not os.path.exists(AUDIO_ROOT):
+        os.makedirs(AUDIO_ROOT)
+    if not os.path.exists(OUTPUT_ROOT):
+        os.makedirs(OUTPUT_ROOT)
+    if not os.path.exists(MODEL_ROOT):
+        os.makedirs(MODEL_ROOT)
+    # Check if the audio file exists
+    audio_file_path = os.path.join(AUDIO_ROOT, file_name)
+
+    if not os.path.exists(audio_file_path):
+        print(f"Audio file {file_name} does not exist in {AUDIO_ROOT}.")
+        return
+
     subprocess.run(
         [
             "whisper",
@@ -24,3 +42,7 @@ def main(model_name: Model, file_name: str):
     for i in os.listdir(OUTPUT_ROOT):
         if i.endswith(".json") or i.endswith(".tsv"):
             os.remove(os.path.join(OUTPUT_ROOT, i))
+
+
+if __name__ == "__main__":
+    main()
